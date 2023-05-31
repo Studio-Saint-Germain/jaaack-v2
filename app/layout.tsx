ab5caa3f86b2a3fe6b873772bb58505ef4c9265b
tsg1986 <hello@tsg1986.com>
2023-05-31 18:51:01 +0200

    First commit : adding navigation component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Navigation from './components/navigation/navigation'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -14,8 +15,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fr">
+      <body className={inter.className}>
+        <Navigation />
+        <main>
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
