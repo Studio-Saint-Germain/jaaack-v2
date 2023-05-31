be70dd543cde28f26b8db642916482c9e37cc946
tsg1986 <hello@tsg1986.com>
2023-05-31 23:04:26 +0200

    Escape

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -9,7 +9,7 @@ export default function About() {
   return (
     <>
       <main className="min-h-screen p-16 md:pl-24 md:mr-">
-        <h1 className="text-[180px] leading-tight text-white font-bold text-right">Hi, I'm <br/>Jaaack.</h1>
+        <h1 className="text-[180px] leading-tight text-white font-bold text-right">Hi, I’m <br/>Jaaack.</h1>
       </main>
       <Footer />
     </>
