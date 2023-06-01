fd6d581f0d3517335a7b7519db1119a91cbd7477
tsg1986 <hello@tsg1986.com>
2023-06-01 19:33:47 +0200

    Video page

--- app/work/[slug]/components/video-player/video-player.tsx
+++ app/work/[slug]/components/video-player/video-player.tsx
@@ -18,25 +18,22 @@ export default function VideoPlayer({ videoInfos }: VideoPlayerProps) {
     const [playing, setPlaying] = useState(false);
     const [showDetails, setShowDetails] = useState(false);
     return (
-        <div className="!w-full !h-screen">
-            <div className='md:relative !w-full !h-screen'>
+        <div className={`!w-full !h-screen ${ showDetails ? 'flex' : ''}`}>
+            <div className={`${ showDetails ? 'bg-white pl-6' : ''} md:relative !w-full !h-screen`}>
                 <ReactPlayer volume={0.5} muted={false} loop={true} responsive={true} playing={playing} className="!w-full !h-full" url={videoInfos.url} />
-                <div className="md:!w-full md:!h-full md:absolute md:top-0 md:flex md:flex-col md:items-start md:justify-between p-6">
-                    <a className="text-white text-2xl cursor-pointer" onClick={() => router.back()}>Back</a>
-                    <div className="md:flex md:justify-between w-full">
-                        <a className="text-white text-2xl cursor-pointer" onClick={() => setPlaying(!playing)}>{playing ? 'Pause' : 'Play'} video</a>
-                        <a className="text-white text-2xl cursor-pointer" onClick={() => setShowDetails(!showDetails)}>Infos</a>
-                    </div>
-                </div>
             </div>
             { videoInfos.description && videoInfos.title && showDetails &&
-                <div className='bg-white'>
-                    <h1 className="text-4xl " dangerouslySetInnerHTML={{__html: videoInfos.title}}></h1>
-                    <div className="" dangerouslySetInnerHTML={{__html: videoInfos.description}}></div>
+                <div className='bg-white h-full overflow-auto p-6 pb-20 text-right'>
+                    <h1 className="text-4xl mb-6" dangerouslySetInnerHTML={{__html: videoInfos.title}}></h1>
+                    <div className="text-sm" dangerouslySetInnerHTML={{__html: videoInfos.description}}></div>
                 </div>
             }
-            <div>
-                
+            <div className={`md:!w-full pointer-events-none md:!h-full md:absolute md:top-0 md:flex md:flex-col md:items-start md:justify-between p-6 ${showDetails ? 'text-black' : 'text-white'}`}>
+                    <a className="text-2xl cursor-pointer pointer-events-auto" onClick={() => router.back()}>Back</a>
+                    <div className="md:flex md:justify-between w-full">
+                        <a className="text-2xl cursor-pointer pointer-events-auto" onClick={() => setPlaying(!playing)}>{playing ? 'Pause' : 'Play'} video</a>
+                        <a className="text-2xl cursor-pointer pointer-events-auto" onClick={() => setShowDetails(!showDetails)}>Infos</a>
+                    </div>
             </div>
         </div>
     )
