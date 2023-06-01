bd052cb4db475cc6dfb713c2d20387df03a1e494
tsg1986 <hello@tsg1986.com>
2023-06-01 15:23:59 +0200

    Add video

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -1,4 +1,7 @@
 import { projectsApi } from "@/app/about/api/projects";
+import dynamic from "next/dynamic";
+import React from 'react';
+const ReactPlayer = dynamic(() => import('react-player/vimeo'), { ssr: false });
 
 export const metadata = {
   title: 'Jack Antoine Charlot - French Director - Work',
@@ -11,6 +14,7 @@ export default async function ProjectSingle({ params }: any) {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-16 md:ml-24">
+        {/* <ReactPlayer url='https://vimeo.com/508768607' /> */}
         <h1 className="text-4xl text-white" dangerouslySetInnerHTML={{__html: project.title.rendered}}></h1>
         <p className="text-white" dangerouslySetInnerHTML={{__html: project.content.rendered}}></p>
       </main>
