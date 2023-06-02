96ed7ff90d56cada42360b992749fa93b7ab8749
tsg1986 <hello@tsg1986.com>
2023-06-02 13:18:21 +0200

    Hover bg

---
+++ app/components/highlighted-projects/highlighted-projects.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { Project, projectsApi } from '@/app/api/projects';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+
+export default function HighlightedProjects() {
+
+  const [highlightedProjects, setHighlightedProjects] = useState<Project[]>();
+  const [background, setBackground] = useState<string>();
+  const [initialBackground, setInitialBackground] = useState<string>('');
+
+  const getHighlightedProjects = async () => {
+    const projects = await projectsApi.getHighlightedProjects();
+    setHighlightedProjects(projects);
+  };
+
+  useEffect(() => {
+    getHighlightedProjects();
+  }, []);
+
+  return (
+    <>
+      <main className="flex min-h-screen flex-col items-center justify-center p-24 !bg-cover" style={background ? {backgroundImage: `url(${background})`} : {}}>
+        <ul className='link-container' onMouseLeave={() => setBackground(initialBackground)}>
+          {highlightedProjects && highlightedProjects.map((project) => (
+            <li className='link-item' key={project.id} onMouseEnter={() => setBackground(project.acf.video_gif)} onMouseLeave={() => setBackground('')}>
+              <Link href={`/work/${project.slug}`}>
+                <p dangerouslySetInnerHTML={{ __html: project.title.rendered }} key={project.id} className='text-white text-center text-xl font-bold cursor-pointer'></p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </main>
+    </>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,18 @@
-'use client';
-
-import Link from 'next/link';
-import { Project, projectsApi } from './api/projects';
 import Footer from './components/footer/footer';
-import { useEffect, useState } from 'react';
-import Head from 'next/head';
+import HighlightedProjects from './components/highlighted-projects/highlighted-projects';
 
-/* export const metadata = {
+export const metadata = {
   title: 'Jack Antoine Charlot | French Director',
   description: 'Jack Antoine Charlot is a french director.',
-} */
+}
 
 export default function Home() {
-
-  const [highlightedProjects, setHighlightedProjects] = useState<Project[]>();
-  const [background, setBackground] = useState<string>();
-
-  const getHighlightedProjects = async () => {
-    const projects = await projectsApi.getHighlightedProjects();
-    setHighlightedProjects(projects);
-  };
-
-  useEffect(() => {
-    getHighlightedProjects();
-  }, []);
 
 
   return (
     <>
-      <Head>
-        <title>Jack Antoine Charlot | French Director</title>
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-center p-24 !bg-cover" style={{backgroundImage: background ? `url(${background})` : 'transparent'}}>
-        <ul className='link-container' onMouseLeave={() => setBackground('')}>
-          {highlightedProjects && highlightedProjects.map((project) => (
-            <li className='link-item' key={project.id} onMouseEnter={() => setBackground(project.acf.video_gif)} onMouseLeave={() => setBackground('')}>
-              <Link href={`/work/${project.slug}`}>
-                <p dangerouslySetInnerHTML={{ __html: project.title.rendered }} key={project.id} className='text-white text-center text-xl font-bold cursor-pointer'></p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </main>
-      <Footer className='absolute' />
+    <HighlightedProjects />
+    <Footer className='absolute' />
     </>
   )
 }
