84fa7bebb6bc4c13fdd93891c98d1e68df55961b
tsg1986 <hello@tsg1986.com>
2023-06-02 15:11:49 +0200

    Bg sizes

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -14,10 +14,10 @@ export default async function About() {
 
   return (
     <>
-          <main className="md:min-h-screen p-16 md:ml-24 page-container bg-cover" style={{background: `linear-gradient(
+          <main className="md:min-h-screen p-16 md:ml-24 page-container !bg-cover" style={pageData.background_image ? {background: `linear-gradient(
     rgba(0, 0, 0, 0.3),
     rgba(0, 0, 0, 0.3)
-  ),url(${pageData.background_image})`}}>
+  ),url(${pageData.background_image} no-repeat`} : {}}>
         <h1 className="page-title mt-16 md:mt-0 md:max-w-[80%] md:ml-[20%] text-white text-right leading-none font-semibold" dangerouslySetInnerHTML={{__html: pageData.title.rendered}}></h1>
         <div className="text-white text-right mt-16 text-xl font-light" dangerouslySetInnerHTML={{__html: pageData.content.rendered}}></div>
         <div className="text-right mt-16">
