f62a4fd7f9c0f327cefd70bb11d5af2903766e3e
tsg1986 <hello@tsg1986.com>
2023-06-05 18:30:01 +0200

    Starts grid

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,14 +1,22 @@
+import { projectsApi } from "../api/projects";
 
 export const metadata = {
   title: 'Jack Antoine Charlot - French Director - Work',
   description: 'Jack Antoine Charlot is a french director.',
 }
 
-export default function Work() {
+export default async function Work() {
+  const projects = await projectsApi.getHighlightedProjects();
   return (
     <>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl">Work</h1>
+      <main className="flex min-h-screen flex-col items-center justify-between py-16 md:pl-24">
+        <div className="md:grid md:grid-cols-6">
+          {projects.map((project, i) => (
+            <div key={project.id} className='text-white text-center text-xl cursor-pointer grid-item'>
+              <p dangerouslySetInnerHTML={{ __html: project.title.rendered }} key={project.id}></p>
+            </div>
+          ))}
+        </div>
       </main>
     </>
   )
