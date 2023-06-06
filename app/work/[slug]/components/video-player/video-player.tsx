5ca0db8eca8cc1a729e93526501f76479e044db2
tsg1986 <hello@tsg1986.com>
2023-06-06 13:55:41 +0200

    smooth scroll to infos on mobile

--- app/work/[slug]/components/video-player/video-player.tsx
+++ app/work/[slug]/components/video-player/video-player.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+import { usePathname, useRouter } from 'next/navigation';
 import { useState } from 'react';
 import ReactPlayer from 'react-player';
 
@@ -15,9 +16,22 @@ export interface VideoInfos {
 
 export default function VideoPlayer({ videoInfos }: VideoPlayerProps) {
     const router = useRouter();
+    const path = usePathname();
     const [playing, setPlaying] = useState(false);
     const [showDetails, setShowDetails] = useState(false);
     const [loading, setLoading] = useState(true);
+    const handleScroll = (e: any, target: string) => {
+        // first prevent the default behavior
+        e.preventDefault();
+        // get the href and remove everything before the hash (#)
+        const href = target;
+        const targetId = href.replace(/.*\#/, "");
+        // get the element by id and use scrollIntoView
+        const elem = document.getElementById(targetId);
+        elem?.scrollIntoView({
+          behavior: "smooth",
+        });
+      };
     
     return (
         <div className={`!w-full !h-screen scroll-smooth ${ showDetails ? 'md:flex' : ''}`}>
@@ -26,16 +40,17 @@ export default function VideoPlayer({ videoInfos }: VideoPlayerProps) {
                 <ReactPlayer volume={0.5} muted={false} onReady={() => setLoading(false)} loop={true} responsive="true" playing={playing} className="!w-full !h-full" url={videoInfos.url} />
             </div>
             { (videoInfos.description || videoInfos.title) && 
-                <div id="video-infos" className={`md:bg-white border-t min-h-screen pt-12 border-t-white text-white md:text-black md:min-h-0 md:h-full overflow-auto p-6 md:pb-20 text-left md:text-right md:max-w-[360px] ${showDetails ? 'md:block':'md:hidden' }`}>
+                <div id="video-infos" className={`md:bg-white border-t min-h-screen pt-24 border-t-white text-white md:text-black md:min-h-0 md:h-full overflow-auto p-6 md:pb-20 text-left md:text-right md:max-w-[360px] ${showDetails ? 'md:block':'md:hidden' }`}>
                     <h1 className="text-2xl font-bold mb-6" dangerouslySetInnerHTML={{__html: videoInfos.title}}></h1>
                     <div className="text-sm description" dangerouslySetInnerHTML={{__html: videoInfos.description}}></div>
                 </div>
             }
             <div className={`!w-full pointer-events-none !h-full absolute top-0 flex flex-col items-start justify-between pt-24 md:pt-6 p-6 pb-0 ${showDetails ? 'md:text-black' : 'text-white'}`}>
-                    <a className="text-2xl cursor-pointer pointer-events-auto" onClick={() => router.back()}>Back</a>
+                    <span className="text-2xl cursor-pointer pointer-events-auto font-light" onClick={() => router.back()}>Back</span>
                     <div className={`flex justify-between w-full py-6 ${ showDetails ? 'md:bg-white' : ''}`}>
-                        <a className="text-2xl cursor-pointer pointer-events-auto" onClick={() => setPlaying(!playing)}>{playing ? 'Pause' : 'Play'} video</a>
-                        <a className="text-2xl cursor-pointer pointer-events-auto hidden md:block" onClick={() => setShowDetails(!showDetails)}>Infos</a>
+                        <span className="text-2xl cursor-pointer font-light pointer-events-auto" onClick={() => setPlaying(!playing)}>{playing ? 'Pause' : 'Play'} video</span>
+                        <span className="text-2xl cursor-pointer font-light pointer-events-auto hidden md:block" onClick={() => setShowDetails(!showDetails)}>Infos</span>
+                        <span onClick={(e) => handleScroll(e, '#video-infos')} className="md:hidden font-light text-2xl pointer-events-auto" scroll={false}>Infos</span>
                     </div>
             </div>
         </div>
