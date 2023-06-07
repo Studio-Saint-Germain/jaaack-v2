3b5262a2fd69c78bb1cbd6ca1c2a6da7af5671c7
tsg1986 <hello@tsg1986.com>
2023-06-07 17:19:50 +0200

    Video bkg on homepage

--- app/components/highlighted-projects/highlighted-projects.tsx
+++ app/components/highlighted-projects/highlighted-projects.tsx
@@ -4,12 +4,13 @@ import { Project, projectsApi } from '@/app/api/projects';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useCallback, useEffect, useState } from 'react';
+import ReactPlayer from 'react-player';
 
 
 export default function HighlightedProjects() {
 
   const [highlightedProjects, setHighlightedProjects] = useState<Project[]>();
-  const [background, setBackground] = useState<string>();
+  const [videoBackground, setVideoBackground] = useState<string>();
 
   const getHighlightedProjects = useCallback( async () => {
     const projects = await projectsApi.getHighlightedProjects();
@@ -23,10 +24,12 @@ export default function HighlightedProjects() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-center pt-24 p-6 md:p-16">
-      {background && <Image src={background} fill={true} className="object-cover object-center" alt="Jaack video preview"></Image>}
-        <ul className='link-container md:ml-16 relative' onMouseLeave={() => setBackground('')}>
+      {videoBackground && 
+      <ReactPlayer muted className="absolute !object-center !object-cover z-0 !w-auto !min-w-full !min-h-full !max-h-none !max-w-none video-preview-player" loop responsive playsinline playing url={videoBackground} />
+      }
+        <ul className='link-container md:ml-16 relative' onMouseLeave={() => setVideoBackground('')}>
           {highlightedProjects && highlightedProjects.map((project) => (
-            <li className='link-item' key={project.id} onMouseEnter={() => setBackground(project.acf.video_gif)} onMouseLeave={() => setBackground('')}>
+            <li className='link-item' key={project.id} onMouseEnter={() => setVideoBackground(project.acf.video_gif)} onMouseLeave={() => setVideoBackground('')}>
               <Link href={`/work/${project.slug}`}>
                 <p dangerouslySetInnerHTML={{ __html: project.title.rendered }} key={project.id} className='text-white text-center p-2 md:p-0 text-xl cursor-pointer'></p>
               </Link>
