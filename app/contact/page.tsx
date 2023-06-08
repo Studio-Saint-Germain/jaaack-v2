ebbe60a7af35adafa26cd7ab3a7a808474c89ef1
tsg1986 <hello@tsg1986.com>
2023-06-08 16:14:32 +0200

    Contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
 import { pagesApi } from "../api/pages";
 import Footer from "../components/footer/footer"
 import VideoFullBackground from "../components/video-full-background/video-full-background";
+import Image from "next/image";
 
 export const metadata = {
   title: 'Jack Antoine Charlot - French Director - Contact',
@@ -19,6 +21,26 @@ export default async function Contact() {
         <div className="relative p-6 md:p-16 md:ml-16">
           <h1 className="page-title mt-16 md:mt-0 text-white text-center leading-none font-semibold" dangerouslySetInnerHTML={{__html: pageData.title.rendered}}></h1>
           <div className="page-description text-white text-center mt-16 text-xl font-light" dangerouslySetInnerHTML={{__html: pageData.content.rendered}}></div>
+          <div className="flex gap-2 items-center justify-center">
+            <Link className="text-3xl text-white underline" href='https://www.instagram.com/jackantoinecharlot' target="_blank">
+            <Image
+                src="/instagram.svg"
+                alt="Jaaack Logo"
+          width={40}
+          height={40}
+          priority
+        />
+            </Link>
+            <Link className="text-3xl text-white underline" href='https://vimeo.com/jackantoinecharlot' target="_blank">
+            <Image
+                src="/vimeo.svg"
+                alt="Jaaack Logo"
+          width={40}
+          height={40}
+          priority
+        />
+            </Link>
+          </div>
         </div>
       </main>
       <Footer className="absolute"/>
