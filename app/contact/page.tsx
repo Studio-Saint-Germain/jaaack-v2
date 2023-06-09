ea163bfd31aeb98080182b0d7c55df3d31d63428
tsg1986 <hello@tsg1986.com>
2023-06-09 16:20:19 +0200

    About update

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -18,9 +18,9 @@ export default async function Contact() {
     <>
       <main className="min-h-screen relative page-container !bg-cover">
         {backgroundVideo && <VideoFullBackground url={backgroundVideo} />}
-        <div className="relative p-6 md:p-16 md:ml-16">
+        <div className="relative p-6 md:p-10 md:ml-16">
           <h1 className="page-title mt-16 md:mt-0 text-white text-center leading-none font-semibold" dangerouslySetInnerHTML={{__html: pageData.title.rendered}}></h1>
-          <div className="page-description text-white text-center mt-16 text-xl font-light" dangerouslySetInnerHTML={{__html: pageData.content.rendered}}></div>
+          <div className="page-description text-white text-center mt-10 text-xl font-light" dangerouslySetInnerHTML={{__html: pageData.content.rendered}}></div>
           <div className="flex gap-2 items-center justify-center">
             <Link className="text-3xl text-white underline" href='https://www.instagram.com/jackantoinecharlot' target="_blank">
             <Image
--- app/work/[slug]/components/video-player/video-player.tsx
+++ app/work/[slug]/components/video-player/video-player.tsx
@@ -1,5 +1,5 @@
 'use client';
-import Link from 'next/link';
+import Image from 'next/image';
 import { usePathname, useRouter } from 'next/navigation';
 import { useState } from 'react';
 import ReactPlayer from 'react-player';
@@ -21,12 +21,9 @@ export default function VideoPlayer({ videoInfos }: VideoPlayerProps) {
     const [showDetails, setShowDetails] = useState(false);
     const [loading, setLoading] = useState(true);
     const handleScroll = (e: any, target: string) => {
-        // first prevent the default behavior
         e.preventDefault();
-        // get the href and remove everything before the hash (#)
         const href = target;
         const targetId = href.replace(/.*\#/, "");
-        // get the element by id and use scrollIntoView
         const elem = document.getElementById(targetId);
         elem?.scrollIntoView({
           behavior: "smooth",
@@ -37,7 +34,7 @@ export default function VideoPlayer({ videoInfos }: VideoPlayerProps) {
         <div className={`!w-full !h-screen scroll-smooth ${ showDetails ? 'md:flex' : ''}`}>
             {loading && <p className="text-white m-auto fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">Loading...</p>}
             <div onClick={() => console.log(!playing)} className={`${ showDetails ? 'md:bg-white md:pl-6' : ''} md:relative !w-full !h-screen ${loading ? 'opacity-0' : 'opacity-100 delay-150'} transition-opacity duration-300`}>
-                <ReactPlayer volume={0.5} muted={false} onReady={() => setLoading(false)} loop={true} responsive="true" playing={playing} className="!w-full !h-full" url={videoInfos.url} />
+                <ReactPlayer onClick={() => console.log('click')} volume={0.5} muted={false} onReady={() => setLoading(false)} loop={true} responsive="true" playing={playing} className="!w-full !h-full" url={videoInfos.url} />
             </div>
             { (videoInfos.description || videoInfos.title) && 
                 <div id="video-infos" className={`md:bg-white border-t min-h-screen pt-24 border-t-white text-white md:text-black md:min-h-0 md:h-full overflow-auto p-6 md:pb-20 text-left md:text-right md:max-w-[360px] ${showDetails ? 'md:block':'md:hidden' }`}>
@@ -46,7 +43,16 @@ export default function VideoPlayer({ videoInfos }: VideoPlayerProps) {
                 </div>
             }
             <div className={`!w-full pointer-events-none !h-full absolute top-0 flex flex-col items-start justify-between pt-24 md:pt-6 p-6 pb-0 ${showDetails ? 'md:text-black' : 'text-white'}`}>
-                    <span className="text-2xl cursor-pointer pointer-events-auto font-light" onClick={() => router.back()}>Back</span>
+                    <span className="text-2xl cursor-pointer pointer-events-auto font-light" onClick={() => router.back()}>
+                      <Image
+                        src="/arrow-back.svg"
+                        alt="back to Jaaack work"
+                        width={80}
+                        className={`${showDetails ? '' : 'invert'}`}
+                        height={20}
+                        priority
+                        />
+                    </span>
                     <div className={`flex justify-between w-full py-6 ${ showDetails ? 'md:bg-white' : ''}`}>
                         <span className="text-2xl cursor-pointer font-light pointer-events-auto" onClick={() => setPlaying(!playing)}>{playing ? 'Pause' : 'Play'} video</span>
                         <span className="text-2xl cursor-pointer font-light pointer-events-auto hidden md:block" onClick={() => setShowDetails(!showDetails)}>Infos</span>
