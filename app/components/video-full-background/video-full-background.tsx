e6a0c0f1f294e285021ba38964fe937b2bc25125
tsg1986 <hello@tsg1986.com>
2023-06-13 00:30:37 +0200

    Play video on mobile

--- app/components/video-full-background/video-full-background.tsx
+++ app/components/video-full-background/video-full-background.tsx
@@ -10,6 +10,6 @@ interface VideoFullBackgroundProps {
 
 export default function VideoFullBackground({url, className, fixed}: VideoFullBackgroundProps) {
   return (
-    <ReactPlayer muted className={`${className ? className : ''} ${fixed ? 'fixed' : 'absolute'} z-0 top-0 !w-auto !min-w-full !min-h-full !max-h-none !max-w-none video-preview-player`} loop playsinline playing url={url} />
+    <ReactPlayer muted className={`${className ? className : ''} ${fixed ? 'fixed' : 'absolute'} z-0 top-0 !w-auto !min-w-full !min-h-full !max-h-none !max-w-none video-preview-player`} loop playsinline autoplay playing url={url} />
   )
 }
