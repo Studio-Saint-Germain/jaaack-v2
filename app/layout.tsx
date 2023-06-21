39498d7cf14fc0f309c5ca8827dc1497229221a2
tsg1986 <hello@tsg1986.com>
2023-06-21 12:20:47 +0200

    Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import localFont from 'next/font/local';
 import Navigation from './components/header/header';
 import './globals.css';
 import { HtmlHead } from './components/htmlHead/htmlHead';
+import { Analytics } from '@vercel/analytics/react';
 
 const satoshi = localFont({
   src: './assets/fonts/satoshi/Satoshi-Variable.woff2',
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body className={satoshi.className}>
         <Navigation />
         {children}
+        <Analytics />
       </body>
     </html>
   )
