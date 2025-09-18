d931820e9de51458109c9fcb2d875e3a4a410be5
tsg1986 <hello@tsg1986.com>
2025-09-18 22:02:03 +0200

    Update: bottom title for snake project

--- app/work/components/project-card/project-card.tsx
+++ app/work/components/project-card/project-card.tsx
@@ -10,27 +10,28 @@ interface ProjectCardProps {
 };
 
 export interface VideoInfos {
-    url: string; 
-    description: string; 
+    url: string;
+    description: string;
     title: string;
 }
 
 export default function ProjectCard({ project }: ProjectCardProps) {
+    console.log(project.id)
     const initialBkg = !!project._embedded['wp:featuredmedia'] ? project._embedded['wp:featuredmedia'][0].source_url : '';
     const [backgroundVideo, setBackgroundVideo] = useState<string>();
     const router = useRouter();
     return (
-        <Link 
-            href={`/work/${project.slug}`} 
-            onMouseEnter={() => setBackgroundVideo(project.acf.video_gif)} 
-            onMouseLeave={() => setBackgroundVideo('')} 
-            style={{backgroundImage: `url(${initialBkg})`}}
+        <Link
+            href={`/work/${project.slug}`}
+            onMouseEnter={() => setBackgroundVideo(project.acf.video_gif)}
+            onMouseLeave={() => setBackgroundVideo('')}
+            style={{ backgroundImage: `url(${initialBkg})` }}
             className='text-white text-center bg-cover block bg-center text-xl cursor-pointer grid-item relative h-49vh overflow-hidden'
         >
             {backgroundVideo && <VideoFullBackground url={backgroundVideo} />}
-            <div className="bg-black bg-opacity-10 !h-full w-full relative flex items-center justify-center p-12">
+            <div className={`bg-black bg-opacity-10 !h-full w-full relative flex ${project.id === 390 ? 'items-end' : 'items-center'} justify-center p-12`}>
                 <p dangerouslySetInnerHTML={{ __html: project.title.rendered }} key={project.id}></p>
             </div>
-        </Link>   
-        )
+        </Link>
+    )
 }
