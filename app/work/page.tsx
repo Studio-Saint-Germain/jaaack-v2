988c3b378085ad524b6d6371c621da6f8493ef6e
tsg1986 <hello@tsg1986.com>
2025-09-18 22:17:20 +0200

    Update

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -13,7 +13,7 @@ export default async function Work() {
       <main className="flex min-h-screen flex-col items-center justify-between md:pl-16 md:py-0">
         <div className="md:grid md:grid-cols-6 w-full gap-0">
           {projects.map((project, i) => (
-            <ProjectCard hasSpecialTitle={i > 4} project={project} key={project.id} />
+            <ProjectCard hasSpecialTitle={i < 4} project={project} key={project.id} />
           ))}
         </div>
       </main>
