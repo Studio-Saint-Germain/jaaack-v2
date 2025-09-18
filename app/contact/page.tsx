f6325d9dbaf3a46e235dbead1d946d516cc70a1b
tsg1986 <hello@tsg1986.com>
2025-09-18 21:14:26 +0200

    Updates: static pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,8 @@
+import Image from "next/image";
 import Link from "next/link";
 import { pagesApi } from "../api/pages";
-import Footer from "../components/footer/footer"
+import Footer from "../components/footer/footer";
 import VideoFullBackground from "../components/video-full-background/video-full-background";
-import Image from "next/image";
 
 export const metadata = {
   title: 'Jack Antoine Charlot - Director & Animation Director - Contact',
@@ -18,31 +18,31 @@ export default async function Contact() {
     <>
       <main className="min-h-screen relative page-container !bg-cover">
         {backgroundVideo && <VideoFullBackground fixed url={backgroundVideo} />}
-        <div className="relative p-6 md:p-10 md:ml-16">
-          <h1 className="page-title mt-16 md:mt-0 text-white text-center leading-none font-semibold" dangerouslySetInnerHTML={{__html: pageData.title.rendered}}></h1>
-          <div className="page-description text-white text-center mt-10 text-xl font-light" dangerouslySetInnerHTML={{__html: pageData.content.rendered}}></div>
-          <div className="flex gap-2 items-center justify-center">
+        <div className="relative p-6 md:p-10 md:ml-16 md:max-w-5xl">
+          <h1 className="page-title mt-16 md:mt-0 text-white leading-none font-semibold" dangerouslySetInnerHTML={{ __html: pageData.title.rendered }}></h1>
+          <div className="page-description text-white mt-10 text-xl font-light" dangerouslySetInnerHTML={{ __html: pageData.content.rendered }}></div>
+          <div className="flex gap-2 items-center">
             <Link className="text-3xl text-white underline" href='https://www.instagram.com/jackantoinecharlot' target="_blank">
-            <Image
+              <Image
                 src="/instagram.svg"
                 alt="Jaaack Logo"
-          width={40}
-          height={40}
-          priority
-        />
+                width={40}
+                height={40}
+                priority
+              />
             </Link>
             <Link className="text-3xl text-white underline" href='https://vimeo.com/jackantoinecharlot' target="_blank">
-            <Image
+              <Image
                 src="/vimeo.svg"
                 alt="Jaaack Logo"
-          width={40}
-          height={40}
-          priority
-        />
+                width={40}
+                height={40}
+                priority
+              />
             </Link>
           </div>
         </div>
-      <Footer className="md:absolute"/>
+        <Footer className="md:absolute" />
       </main>
     </>
   )
