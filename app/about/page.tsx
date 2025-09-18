65132f96a16d369f8f2c9f6aa245c9f45735b8a8
tsg1986 <hello@tsg1986.com>
2025-09-18 21:07:26 +0200

    Updates

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import { pagesApi } from "../api/pages"
-import Footer from "../components/footer/footer"
+import { pagesApi } from "../api/pages";
+import Footer from "../components/footer/footer";
 import VideoFullBackground from "../components/video-full-background/video-full-background";
 
 export const metadata = {
@@ -13,19 +13,20 @@ const ABOUT_PAGE_ID = 16;
 export default async function About() {
   const pageData = await pagesApi.getPageById(ABOUT_PAGE_ID);
   const backgroundVideo = pageData.acf.background_video;
+  console.log(pageData)
 
   return (
     <>
       <main className="min-h-screen relative page-container">
-      {backgroundVideo && <VideoFullBackground fixed url={backgroundVideo} />}
+        {backgroundVideo && <VideoFullBackground fixed url={backgroundVideo} />}
         <div className="relative p-6 pt-20 md:p-10 md:ml-16">
-        <h1 className="page-title mt-16 md:mt-0 text-white text-center leading-none font-semibold" dangerouslySetInnerHTML={{__html: pageData.title.rendered}}></h1>
-        <div className="text-white page-description mt-10 text-xl text-center font-light" dangerouslySetInnerHTML={{__html: pageData.content.rendered}}></div>
-        <div className="text-center mt-16">
-          <Link className="text-xl text-white underline" href='/contact'>Contact & booking</Link>
+          <h1 className="page-title mt-16 md:mt-0 text-white text-center leading-none font-semibold" dangerouslySetInnerHTML={{ __html: pageData.title.rendered }}></h1>
+          <div className="text-white page-description mt-10 text-xl font-light" dangerouslySetInnerHTML={{ __html: pageData.content.rendered }}></div>
+          <div className="text-center mt-16">
+            <Link className="text-xl text-white underline" href='/contact'>Contact & booking</Link>
+          </div>
         </div>
-        </div>
-      <Footer className="md:absolute"/>
+        <Footer className="md:absolute" />
       </main>
     </>
   )
